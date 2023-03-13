6a4a0a66d966bf0f2cf8128ba933a01623ba95f8
İpek Ülfet Kaylan <ipekulfetkaylan@gmail.com>
2023-03-13 17:02:02 +0300

    ıcons eklendi

--- axios/icons/src/packages/Icons.jsx
+++ axios/icons/src/packages/Icons.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { FaBeer, FaMagento, FaPen } from "react-icons/fa";
+import {IconContext} from "react-icons";
+import {IoIosAlert} from 'react-icons/io';
+import {GiPencil} from "react-icons/gi"
 // fontawesomedan bir icon çektik
 
 
@@ -10,6 +13,20 @@ function Icons() {
       <FaPen size="60px" color='tomato'/>
       <FaBeer size="60" color="green"/>
       <FaMagento size="60" color="pink"/>
+      <IconContext.Provider value={{color: "pink", size:"20px"}}>
+        {/* eger paketlerin hepsini kullanmak istiyorsak ıconContextten alıyoruz ve provider ekliyoruz */}
+        <p>
+            <IoIosAlert style={{color: "red", size:"30px"}}/>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi iure, velit nam eum pariatur rerum quaerat exercitationem aspernatur maiores id fugit 
+            saepe quas aliquid numquam voluptate soluta, nihil tempora. Quos nulla provident nam quis explicabo debitis animi architecto quibusdam alias 
+            aliquam, maiores omnis id aut quaerat laboriosam quae nesciunt aperiam.
+            
+        </p>
+        <GiPencil style={{color: "black", size:"20px"}} />
+
+        <input placeholder='Edit'></input>
+
+      </IconContext.Provider>
     </div>
 
   )
